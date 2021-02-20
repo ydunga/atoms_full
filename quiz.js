a90f410a14b7f04f4b2c0997deb115a3d7a2027b
ydunga <55959066+ydunga@users.noreply.github.com>
2021-02-20 19:20:25 +0000

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,61 @@
+function myFunction() {
+    var  inpObj = document.getElementById("id1");
+    if((inpObj.value == "Aluminium Sulfate")) {
+      document.getElementById('err').innerHTML = 'correct';
+     
+    }
+    else {
+      document.getElementById('err').innerHTML = 'try again';
+    }
+    
+  }
+  
+  function myFunction2() {
+    var  inpObj2 = document.getElementById("id2");
+    if((inpObj2.value == "Calcium Chloride")) {
+      document.getElementById('err2').innerHTML = 'correct';
+     
+    }
+    else {
+      document.getElementById('err2').innerHTML = 'try again';
+    }
+    
+  }
+  
+  function myFunction3() {
+    var  inpObj3 = document.getElementById("id3");
+    if((inpObj3.value == "Ammonium Chloride")) {
+      document.getElementById('err3').innerHTML = 'correct';
+     
+    }
+    else {
+      document.getElementById('err3').innerHTML = 'try again';
+    }
+    
+  }
+  
+  function myFunction4() {
+    var  inpObj4 = document.getElementById("id4");
+    if((inpObj4.value == "Potassium Nitrate")) {
+      document.getElementById('err4').innerHTML = 'correct';
+     
+    }
+    else {
+      document.getElementById('err4').innerHTML = 'try again';
+    }
+    
+  }
+  
+  function myFunction5() {
+    var  inpObj5 = document.getElementById("id5");
+    if((inpObj5.value == "Calcium Carbonate")) {
+      document.getElementById('err5').innerHTML = 'correct';
+     
+    }
+    else {
+      document.getElementById('err5').innerHTML = 'try again';
+    }
+    
+  }
+  
+  
